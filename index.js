9c1264f3c2855b7a6bf07e5433b340d31e24258a
Celina Dominique <128432725+selinacomc@users.noreply.github.com>
2024-10-30 21:29:14 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+let pokemons = [
+    [ "POOCHVENA", 2, "M" ],
+    [ "ZIGZAGOON", 2, "F" ],
+    [ "DRAGONITE", 5, "M" ],
+    [ "DRAGONITE", 5, "F" ],
+    [ "POOCHVENA", 3, "F" ],
+    [ "HURMPLE",  2, "M" ]
+]
+
+
+console.log("O pokemon " + pokemons [0][0] + " do nível " + pokemons [0][1] + " do gênero " + pokemons [0][2] + " foi cadastrado com sucesso." )
+
+console.log("O pokemon " + pokemons [1][0] + " do nível " + pokemons [1][1] + " do gênero " + pokemons [1][2] + " foi cadastrado com sucesso." )
+
+console.log("O pokemon " + pokemons [2][0] + " do nível " + pokemons [2][1] + " do gênero " + pokemons [2][2] + " foi cadastrado com sucesso." )
+
+console.log("O pokemon " + pokemons [3][0] + " do nível " + pokemons [3][1] + " do gênero " + pokemons [3][2] + " foi cadastrado com sucesso." )
+
+console.log("O pokemon " + pokemons [4][0] + " do nível " + pokemons [4][1] + " do gênero " + pokemons [4][2] + " foi cadastrado com sucesso." )
+
+console.log("O pokemon " + pokemons [5][0] + " do nível " + pokemons [5][1] + " do gênero " + pokemons [5][2] + " foi cadastrado com sucesso." )
